991fb98b017ce40e47e77986a975b9775a036351
Charpell <charpellumeh@gmail.com>
2018-06-26 22:50:36 +0100

    Reload file when changed with nodemon and --watch command

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>I'm the home component</div>;
+  return <div>I'm the home component and i have changed</div>;
 };
 export default Home;
